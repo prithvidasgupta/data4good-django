c3a2e151a7d5b6179e99fe936e0baf2fec766330
prithvidasgupta <prithvid@umich.edu>
2022-12-11 21:33:51 -0500

    chetah_v1 api

--- web/urls.py
+++ web/urls.py
@@ -2,13 +2,12 @@
 from django.urls import path
 from django.contrib import admin
 from . import views
-from django.views.generic import TemplateView
-# from web.views import ContributeView
 
 app_name='web'
 urlpatterns = [
     path('', views.MainView.as_view(), name='all'),
     path('projects/cio4good', views.CIO4GoodView.as_view(), name='cio4good'),
     path('projects/chetah', views.ChetahView.as_view(), name='chetah'),
-    path('api/chetah/v1/<str:query>', views.chetah_search)
+    path('projects/simex', views.SimexView.as_view(), name='simex'),
+    path('api/chetah/v1/<str:query>', views.chetah_search, name='chetah_v1_api')
 ]
--- web/views.py
+++ web/views.py
@@ -10,59 +10,21 @@ from django.template.loader import render_to_string
 
 from web.models import Task, Project
 
-# PDF Parsing
-import pandas as pd
-import numpy as np
-import json
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy import sparse
+from .chetah_v1 import search as chetahV1Search
 
+# PDF Parsin
 # Prepare dataframe
 #Uncomment in test, comment in prod
-df_pdfs = pd.read_csv('final_with_cluster.csv')
-#Uncomment in prod, comment in test
-#df_pdfs = pd.read_csv('/home/D4GUMSI/data4good-django/final_with_cluster.csv')
-            
-# Extract summaries from PDFs and queries from query list
-summaries = [x for x in df_pdfs.summary]
-# queries = [x for x in df_queries.Query]
 
-vectorizer = TfidfVectorizer()
-class BM25(object):
-    def __init__(self, b=0.75, k1=1.6):
-        self.vectorizer = TfidfVectorizer(norm=None, smooth_idf=False)
-        self.b = b
-        self.k1 = k1
-
-    def fit(self, X):
-        """ Fit IDF to documents X """
-        self.vectorizer.fit(X)
-        y = super(TfidfVectorizer, self.vectorizer).transform(X)
-        self.avdl = y.sum(1).mean()
-
-    def transform(self, q, X):
-        """ Calculate BM25 between query q and documents X """
-        b, k1, avdl = self.b, self.k1, self.avdl
-
-        # apply CountVectorizer
-        X = super(TfidfVectorizer, self.vectorizer).transform(X)
-        len_X = X.sum(1).A1
-        q, = super(TfidfVectorizer, self.vectorizer).transform([q])
-        assert sparse.isspmatrix_csr(q)
-
-        # convert to csc for better column slicing
-        X = X.tocsc()[:, q.indices]
-        denom = X + (k1 * (1 - b + b * len_X / avdl))[:, None]
-        idf = self.vectorizer._tfidf.idf_[None, q.indices] - 1.
-        numer = X.multiply(np.broadcast_to(idf, X.shape)) * (k1 + 1)                                                          
-        return (numer / denom).sum(1).A1
-
-bm25 = BM25()
-bm25.fit(summaries)
 
 class MainView(TemplateView) :
     def get(self, request):
         return render(request, 'web/home.html')
+
+
+class SimexView(TemplateView) :
+    def get(self, request) :
+        return render(request, "web/project_simex.html")#, context)
 
 class CIO4GoodView(TemplateView) :
     def get(self, request) :
@@ -86,71 +48,11 @@ class ChetahView(TemplateView) :
             }
             query = request.POST['search-query']
             context['search_query'] = query
-            
-            query_sample = bm25.transform(query, summaries)
 
-            weights = []
-            for i in query_sample:
-                if i > 1:
-                    weights.append(i)
-
-            sorted_top = sorted(weights, key = lambda x: x, reverse = True)[:10]
-
-            sorted_top_i = [np.where(query_sample == i) for i in sorted_top]
-            top_indexes = [x[0][0] for x in sorted_top_i]
-
-            for i in top_indexes:
-                # Process the clusters associated with the PDF
-                clusters_list = []
-
-                # Create a new PDF dictionary and add it to the list of search results
-                pdf = {
-                    "title": str(df_pdfs.Title[i]),
-                    "date": df_pdfs.Date[i],
-                    "link": str(df_pdfs.URL[i]),
-                    "cluster": str(df_pdfs.cluster[i]),
-                    "summary_short": str(df_pdfs.summary[i])[:450] + "...", # Truncate summary after 450 characters
-                    "summary_full": str(df_pdfs.summary[i]),
-                }
-                context['search_results'].append(pdf)             
+            context['search_results'] = chetahV1Search(query)             
 
             return render(request, "web/project_chetah.html", context)
 
 
-# class DataSetsView(TemplateView) :
-#     def get(self, request):
-#         return render(request, 'web/datasets.html')#, ctx)
-
-# class ContributeView(TemplateView) :
-#     template_name = "web/contribute.html"
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['task_list'] = Task.objects.all()
-#         return context
-
 def chetah_search(request, query):
-    query_sample = bm25.transform(query, summaries)
-    results = []
-
-    weights = []
-    for i in query_sample:
-        if i > 1:
-            weights.append(i)
-
-    sorted_top = sorted(weights, key = lambda x: x, reverse = True)[:10]
-    sorted_top_i = [np.where(query_sample == i) for i in sorted_top]
-    top_indexes = [x[0][0] for x in sorted_top_i]
-
-    for i in top_indexes:
-        # Process the clusters associated with the PDF
-        clusters_list = []
-
-        # Create a new PDF dictionary and add it to the list of search results
-        pdf = {
-                "title": str(df_pdfs.Title[i]),
-                "date": df_pdfs.Date[i],
-                "link": str(df_pdfs.URL[i]),
-                "cluster": str(df_pdfs.cluster[i])
-                }
-        results.append(pdf)
-    return JsonResponse({ 'search_results': results })
+    return JsonResponse({ 'search_results': chetahV1Search(query) })
